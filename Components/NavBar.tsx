3459f6935de27d794e91d8dff2c85d48a56a2bb9
conormcfarlane <conormcf98@gmail.com>
2026-02-26 18:41:22 +1100

    nav bar and dark mode made

--- Components/NavBar.tsx
+++ Components/NavBar.tsx
@@ -1,13 +1,14 @@
 "use client";
-
-import React from "react";
+import { useState } from "react";
 import Image from "next/image";
 import imageAvatar from "../public/images/image-avatar.jpg";
 import iconHamburgerMenu from "../public/images/icon-menu.svg";
+import iconMenuClose from "../public/images/icon-menu-close.svg";
 import iconMoon from "../public/images/icon-moon.svg";
 import iconSun from "../public/images/icon-sun.svg";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useTheme } from "next-themes";
 
 export default function NavBar() {
   const sitePages = [
@@ -19,38 +20,77 @@ export default function NavBar() {
 
   const pathName = usePathname();
   const isPathEqual = (href: string) => pathName === href;
+
+  const [isActive, setIsActive] = useState(false);
+  const { theme, setTheme, resolvedTheme } = useTheme();
   return (
-    <div className="bg-red-500 flex justify-between items-center p-1.5">
-      <div>
-        <Image
-          src={imageAvatar}
-          alt="avatar iamge"
-          height={40}
-          width={40}
-          className="rounded-lg"
-        />
-      </div>
-      <div className="flex items-center gap-1.5">
-        {/* Pages Links Medium + Large */}
-        <div className="gap-6 mr-5 hidden md:flex">
-          {sitePages.map((sitePage) => {
-            return <Link href={sitePage.href}>{sitePage.name}</Link>;
-          })}
-        </div>
-        {/* Hamburger for small */}
-        <div className="p-2.5 block md:hidden">
+    <>
+      <div className="flex justify-between items-center p-1.5 border-container bg-white">
+        <div>
           <Image
-            src={iconHamburgerMenu}
-            alt="menu button"
-            height={20}
-            width={20}
+            src={imageAvatar}
+            alt="avatar iamge"
+            height={40}
+            width={40}
+            className="rounded-lg"
           />
         </div>
+        <div className="flex items-center gap-1.5">
+          {/* Pages Links Medium + Large */}
+          <div className="gap-6 mr-5 hidden md:flex">
+            {sitePages.map((sitePage) => {
+              return (
+                <Link key={sitePage.href} href={sitePage.href}>
+                  {sitePage.name}
+                </Link>
+              );
+            })}
+          </div>
+          {/* Hamburger for small */}
+          <div
+            className={`p-2.5 block md:hidden rounded-lg ${isActive ? "bg-black" : "bg-white"}`}
+          >
+            <Image
+              src={isActive ? iconMenuClose : iconHamburgerMenu}
+              alt="menu button"
+              height={20}
+              width={20}
+              onClick={() => setIsActive((prevMode) => !prevMode)}
+              className="cursor-pointer"
+            />
+          </div>
 
-        <button className="p-2.5 border rounded-lg">
-          <Image src={iconSun} alt="light mode button" height={20} width={20} />
-        </button>
+          <button
+            className="p-2.5 border-container cursor-pointer dark:bg-black"
+            onClick={() => {
+              setTheme(resolvedTheme === "dark" ? "light" : "dark");
+              console.log(resolvedTheme);
+            }}
+          >
+            <Image
+              src={resolvedTheme === "dark" ? iconSun : iconMoon}
+              alt="light mode button"
+              height={20}
+              width={20}
+            />
+          </button>
+        </div>
       </div>
-    </div>
+      {isActive && (
+        <div className="flex flex-col border-container mt-3 px-3">
+          {sitePages.map((sitePage) => {
+            return (
+              <Link
+                key={sitePage.href}
+                href={sitePage.href}
+                className={`py-2 ${sitePage.name != "Newsletter" ? "border-b border-[#424242]" : ""}`}
+              >
+                {sitePage.name}
+              </Link>
+            );
+          })}
+        </div>
+      )}
+    </>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import NavBar from "@/Components/NavBar";
+import { ThemeProvider } from "next-themes";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,13 +25,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased p-4`}
       >
-        <header><NavBar /></header>
-        <main> {children}</main>
-        <footer></footer>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <header>
+            <NavBar />
+          </header>
+          <main> {children}</main>
+          <footer></footer>
+        </ThemeProvider>
       </body>
     </html>
   );
